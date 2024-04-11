15fe3ae18fabbace1e6ffe0e26f5b0e244f5bdab
Adam <adam5596@stud.kea.dk>
2024-04-11 14:14:52 +0200

    new redis cache server working

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os
+import redis
+from redis.exceptions import ConnectionError
 import requests
-from urllib.parse import urlencode
+import json
+import os
+import logging
+
 
 from auth import get_access_token
 
 load_dotenv()
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+app.debug = True
 CORS(app)
 
 if __name__ == "__main__":
@@ -18,19 +24,44 @@ if __name__ == "__main__":
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World (of Warcraft)!"
+
+
+r = redis.from_url(os.getenv("REDIS_URL"))
+
+
+@app.route("/clear_redis", methods=["POST"])
+def clear_redis():
+    r.flushall()
+    return "OK"
+
+
+@app.route("/get_redis", methods=["GET"])
+def get_redis_keys():
+    keys = r.keys()
+    keys = [key.decode("utf-8") for key in keys]  # type: ignore
+    return jsonify(json.dumps(keys))
 
 
 @app.route("/get_character_data", methods=["POST"])
 def get_character_data():
-
     data = request.get_json()
     name = data.get("name")
     server = data.get("server")
     region = data.get("region")
     zone = data.get("zone")
 
-    print(name, server, region, zone)
+    key = f"{name}:{server}:{region}:{zone}"
+
+    try:
+        character_data = r.get(key)
+    except ConnectionError:
+        app.logger.error("Unable to connect to Redis")
+
+    if character_data is not None:
+        app.logger.info("Data found in Redis")
+        character_data = character_data.decode("utf-8")  # type: ignore
+        return jsonify(json.loads(character_data))
 
     query = f"""
     query {{
@@ -52,8 +83,8 @@ def get_character_data():
         json={"query": query},
     )
     result = response.json()
-
     character_data = result.get("data", {}).get("characterData", {}).get("character")
+
     if not character_data:
         character_data = {
             "classID": 1,
@@ -74,9 +105,16 @@ def get_character_data():
             },
         }
 
-    return jsonify(
+    finishedObject = jsonify(
         {
             "name": name,
-            "result": character_data,
+            str(zone): {
+                "result": character_data,
+            },
         }
     )
+
+    r.set(key, json.dumps(finishedObject.get_json()))
+    r.expire(key, 3600)
+
+    return finishedObject
--- auth.py
+++ auth.py
@@ -1,5 +1,3 @@
-from flask import Flask, request, jsonify
-from dotenv import load_dotenv
 import os
 import requests
 from urllib.parse import urlencode
